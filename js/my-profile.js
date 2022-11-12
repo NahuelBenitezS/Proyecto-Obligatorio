87188e424b555a3dfca2aa30fb9a724806ca3d72
Nahuel Benitez <nahuelbenitezs2002@gmail.com>
2022-11-12 15:44:49 -0300

    my profile

--- js/my-profile.js
+++ js/my-profile.js
@@ -8,44 +8,42 @@ let phone = document.getElementById("phone");
 let imgProfile = document.getElementById("imgProfile");
 let img = document.getElementById("img");
 let data = {};
-datauser = JSON.parse(localStorage.getItem(user))
+datauser = JSON.parse(localStorage.getItem(user));
 email.value = localStorage.getItem("user");
 
-
 // mostrar datos guardados
-if (datauser !== null){
-    firstName.value = datauser.firstName;
-    secondName.value = datauser.secondName;
-    firstSurname.value = datauser.firstSurname;
-    secondSurname.value = datauser.secondSurname;
-    phone.value = datauser.phone;
-    img.src = localStorage.getItem("image");
-    if (datauser.imgProfile !== ''){
-        img.src = datauser.imgProfile
-    } 
+if (datauser !== null) {
+  firstName.value = datauser.firstName;
+  secondName.value = datauser.secondName;
+  firstSurname.value = datauser.firstSurname;
+  secondSurname.value = datauser.secondSurname;
+  phone.value = datauser.phone;
+  //imagen del gmail
+  img.src = localStorage.getItem("image");
+  if (datauser.imgProfile !== "") {
+    img.src = datauser.imgProfile;
+  } else {
+  }
 }
 
-// let productCartID = JSON.parse(localStorage.getItem("productCartID"));
+formPerfil.addEventListener("submit", saveChanges);
 
-
-formPerfil.addEventListener('submit', saveChanges);
-
+// guardar datos en el localStorage con el nombre de usuario
 function saveChanges() {
-    
-    data.firstName = firstName.value;
-    data.secondName = secondName.value;
-    data.firstSurname = firstSurname.value;
-    data.secondSurname = secondSurname.value;
-    data.phone = phone.value;
-    data.email = email.value;
-    data.imgProfile = imgProfile.value
-    localStorage.setItem(user, JSON.stringify(data));
-};
+  data.firstName = firstName.value;
+  data.secondName = secondName.value;
+  data.firstSurname = firstSurname.value;
+  data.secondSurname = secondSurname.value;
+  data.phone = phone.value;
+  data.email = email.value;
 
-if (localStorage.getItem(user) > 0){
-    
-    console.log("hola");
+  reader.readAsDataURL(imgProfile.files[0]);
+
+ 
 }
-
-
-
+const reader = new FileReader();
+reader.onloadend = function (event) {
+    data.imgProfile = event.target.result;
+    img.src = event.target.result;
+    localStorage.setItem(user, JSON.stringify(data));
+  };
